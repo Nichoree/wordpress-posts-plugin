cea2b6e716914a8257b9b2ba7cb43913681db186
Navish <navish45@gmail.comm>
2022-02-09 23:16:17 +0300

    remove activation and deactivation hooks

--- posts-list.php
+++ posts-list.php
@@ -17,55 +17,47 @@
  *      "posts" in post-type can be replaced by custom post types
  * 
 */
-
 function posts_list_shortcode($atts)
-{
-    ob_start();
-    $defaults = array(
-        'post-type' => 'posts',
-        'category' => 0,
-        'number-of-posts' => -1
-    );
-    $post_atts = shortcode_atts($defaults, $atts, 'posts-list');
-    $posts_args = array(
-        'numberposts' => $post_atts['number-of-posts'],
-        'post_type' => $post_atts['post-type'],
-        'category_name' => $post_atts['category'],
-    );
+    {
+        ob_start();
+        $defaults = array(
+            'post-type' => 'posts',
+            'category' => 0,
+            'number-of-posts' => -1
+        );
+        $post_atts = shortcode_atts($defaults, $atts, 'posts-list');
+        $posts_args = array(
+            'numberposts' => $post_atts['number-of-posts'],
+            'post_type' => $post_atts['post-type'],
+            'category_name' => $post_atts['category'],
+        );
 
-    $posts = get_posts($posts_args);
+        $posts = get_posts($posts_args);
 
-    if ($posts) { ?>
-        <div class="w3-row-padding post-row">
-        <?php 
-        foreach($posts as $post) { ?>
-        
-            <div class="w3-col l4 m6 s12">
-          <a href=<?php echo get_permalink($post->ID); ?>>
-              <span class="small-post-image">
-                <?php echo get_the_post_thumbnail($post->ID); ?>
-              </span>
-              <h4><?php echo esc_html( get_the_title($post->ID) ); ?></h4>
-          </a>
-        </div> <?php
-        }
-     };
-        
-    // always return
-    return ob_get_clean();
+        if ($posts) { ?>
+            <div class="w3-row-padding post-row">
+            <?php 
+            foreach($posts as $post) { ?>
+            
+                <div class="w3-col l4 m6 s12">
+                <a href=<?php echo get_permalink($post->ID); ?>>
+                <span class="post-image">
+                    <?php echo get_the_post_thumbnail($post->ID); ?>
+                </span>
+                <h4><?php echo esc_html( get_the_title($post->ID) ); ?></h4>
+            </a>
+            </div> 
+            <?php } ?>
+            </div>
+            <?php
+        } else { echo "<p>No posts found </p>"; }
+            
+        // always return
+        return ob_get_clean();
+    }
 
-}
+// Register shortcode
+add_shortcode('posts-list', 'posts_list_shortcode');
 
-// On plugin activation
-function posts_list_activate(){
-    // Register shortcode
-    add_shortcode('posts-list', 'posts_list_shortcode');
-}
-register_activation_hook(__FILE__, 'posts_list_activate');
 
-//On plugin deactivation
-function posts_list_deactivate(){
-    remove_shortcode('posts-list');
-}
-register_deactivation_hook(__FILE__, 'posts_list_activate');
 
